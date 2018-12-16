1899990add0c4e4a16cb20ad9edcc57c335d58ed
LucasX <xulu0620@gmail.com>
2018-12-16 23:46:35 +0800

    rewrite train/test code

--- analysis/zhihu_live_regressor.py
+++ analysis/zhihu_live_regressor.py
@@ -15,6 +15,7 @@ from sklearn.neural_network import MLPRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.svm import SVR
 from sklearn.externals import joblib
+from sklearn.model_selection import train_test_split
 
 import torch
 from torch.optim import lr_scheduler
@@ -29,10 +30,10 @@ sys.path.append('../')
 from analysis.models import MTBDNN, MLP
 
 
-def split_train_test(excel_path, test_ratio, dl=True):
-    # df = pd.read_excel(excel_path, sheetname="Preprocessed").fillna(value=0)
-    df = pd.read_excel(excel_path, sheetname="Preprocessed")
-    # df = df[df['review_score'] > 0]
+def split_train_test(excel_path, dl=True):
+    # df = pd.read_excel(excel_path, sheet_name="ZhihuLive").fillna(value=0)
+    df = pd.read_excel(excel_path, sheet_name="ZhihuLive")
+    df = df[df['review_count'] >= 11]
     print("*" * 100)
     if not dl:
         dataset = df.loc[:, ['duration', 'reply_message_count', 'source', 'purchasable', 'is_refundable',
@@ -85,14 +86,9 @@ def split_train_test(excel_path, test_ratio, dl=True):
         labels = df.loc[:, ['review_score']]
 
     print("*" * 10)
+    X_train, X_test, y_train, y_test = train_test_split(dataset, labels, random_state=42)
 
-    shuffled_indices = np.random.permutation(len(df))
-    test_set_size = int(len(df) * test_ratio)
-    test_indices = shuffled_indices[:test_set_size]
-    train_indices = shuffled_indices[test_set_size:]
-
-    return dataset.iloc[train_indices], dataset.iloc[test_indices], \
-           labels.iloc[train_indices], labels.iloc[test_indices]
+    return X_train, X_test, y_train, y_test
 
 
 def train_and_test_model(train, test, train_Y, test_Y):
@@ -101,18 +97,18 @@ def train_and_test_model(train, test, train_Y, test_Y):
 
     # model = LassoCV(alphas=[_ * 0.1 for _ in range(1, 1000, 1)])
     # model = RidgeCV(alphas=[_ * 0.1 for _ in range(1, 1000, 1)])
-    # model = SVR(kernel='rbf', C=1e3, gamma=0.1)
+    model = SVR(kernel='rbf', C=1e3, gamma=0.1)
     # model = SVR(kernel='linear', C=1e3)
     # model = SVR(kernel='poly', C=1e3, degree=2)
     # model = KNeighborsRegressor(n_neighbors=10, n_jobs=4)
 
-    model = MLPRegressor(hidden_layer_sizes=(16, 8, 8), early_stopping=True, alpha=1e-4,
-                         batch_size=16, learning_rate='adaptive')
-    model.fit(train, train_Y)
+    # model = MLPRegressor(hidden_layer_sizes=(16, 8, 8, 4), early_stopping=True, alpha=1e-4,
+    #                      batch_size=16, learning_rate='adaptive')
+    model.fit(train, train_Y.values.ravel())
     predicted_score = model.predict(test)
     mae_lr = round(mean_absolute_error(test_Y, predicted_score), 4)
     rmse_lr = round(np.math.sqrt(mean_squared_error(test_Y, predicted_score)), 4)
-    # pc = round(np.corrcoef(test_Y, predicted_score)[0, 1], 4)
+    # pc = round(np.corrcoef(test_Y.values.ravel(), predicted_score)[0, 1], 4)
     print('===============The Mean Absolute Error is {0}===================='.format(mae_lr))
     print('===============The Root Mean Square Error is {0}===================='.format(rmse_lr))
     # print('===============The Pearson Correlation of Model is {0}===================='.format(pc))
@@ -135,7 +131,7 @@ def feature_selection(X, y, k=15):
     return X, y
 
 
-def mtb_dnns(train, test, train_Y, test_Y, epoch=100):
+def train_and_test_mtb_dnns(train, test, train_Y, test_Y, epoch=100):
     """
     train and test with MTB-DNN
     :param train:
@@ -176,7 +172,7 @@ def mtb_dnns(train, test, train_Y, test_Y, epoch=100):
     optimizer = optim.Adam(mtbdnn.parameters(), lr=0.001, weight_decay=1e-3)
     # learning_rate_scheduler = lr_scheduler.ExponentialLR(optimizer, gamma=0.5)
 
-    for epoch in range(epoch):  # loop over the dataset multiple times
+    for epoch in range(epoch):
 
         running_loss = 0.0
         for i, data_batch in enumerate(trainloader):
@@ -282,8 +278,8 @@ def predict_score(zhihu_live_id):
 
 
 if __name__ == '__main__':
-    train_set, test_set, train_label, test_label = split_train_test("./ZhihuLiveDB.xlsx", 0.2)
-    # train_and_test_model(train_set, test_set, train_label, test_label)
-    mtb_dnns(train_set, test_set, train_label, test_label, 30)
+    train_set, test_set, train_label, test_label = split_train_test("../spider/ZhihuLiveDB.xlsx", 0.2)
+    train_and_test_model(train_set, test_set, train_label, test_label)
+    # train_and_test_mtb_dnns(train_set, test_set, train_label, test_label, 30)
 
     # predict_score('788099469471121408')
